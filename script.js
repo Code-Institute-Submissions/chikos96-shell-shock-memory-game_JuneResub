cd23a2bef67c21da0340c6ceab306c2a70533193
chikos96 <tchikos741@gmail.com>
2020-11-09 14:52:28 +0000

    Created script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let card = document.querySelectorAll;
+let moves = 0;
+let time = 0;
+let cardsMatched =0;
+let cardsUnMatches =0;
+let cardsClicked = [];
+let cardsFlipped = false;
+let shellBoard = false;
+let cardShellOne, cardShellTwo
+
+function gameReset() {
+    moves = 0;
+    time = 0;
+    cardsMatched = 0;
+    cardsUnMatches = 0;
+    cardsClicked = [];
+}
+
+
+ 
+
