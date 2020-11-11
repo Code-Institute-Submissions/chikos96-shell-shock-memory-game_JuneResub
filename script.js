4b407100a6960a31d86f0322e6fa6fc1e65c1afb
chikos96 <tchikos741@gmail.com>
2020-11-11 10:29:10 +0000

    Testing the code

--- script.js
+++ script.js
@@ -1,63 +1,60 @@
-let card = document.querySelectorAll;
-let moves = 0;
-let time = 0;
-let cardsMatched =0;
-let cardsUnMatched =0;
-let cardsClicked = [];
+let card = document.querySelectorAll("shell-card");
+
 let cardsFlipped = false;
-let shellBoard = false;
-let cardShellOne, cardShellTwo
+let lockBoard = false;
+let cardShellOne, cardShellTwo;
+
+function cardFlip () {
+    if (lockBoard) return;
+    if (this === cardShellOne) return;
+
+    this.classList.toggle('flip')
+                        
+    if (cardsFlipped)   {
+        cardsFlipped = true;
+        cardShellOne = this;
+
+        return;
+    }
+    
+        cardShellTwo = this;
+        checkCardsMatch();
+}
+
+function checkCardsMatch () {
+    let match = cardShellOne.dataset.name === cardShellTwo.dataset.name
+
+    match ? disableCards : cardsUnflipped
+}
+
+function disableCards () {
+    cardShellOne.removeEventListener('click', cardFlip)
+     cardShellTwo.removeEventListener('click', cardFlip)
+}
+
+function cardsUnflipped () {
+    lockBoard = true;
+
+    setTimeout(() => {
+        cardShellOne.classList.remove('flip')
+        cardShellTwo.classList.remove('flip')
+
+
+        gameReset()
+    }, 2500);
+
+}
 
 function gameReset() {
-    moves = 0;
-    time = 0;
-    cardsMatched = 0;
-    cardsUnMatches = 0;
-    cardsClicked = [];
+    [shellBoard, cardsFlipped] = [false, false]
+    [cardShellOne, cardShellTwo] = [false, false]
 }
 
-function cardsFlipped() {
-    if (shellBoard) return; {
-        if (this === cardShellOne) return;
+(function cardShuffle () {
+    card.forEach(card => {
+        let randomCard = Math.floor(Math.random() * 12)
+        card.style.order = randomCard
+    })
+})
 
-        this.classList.add('flip')
-    }
-    cardShellTwo = this
-    cardsMatched()
-}
-
-function cardsMatched () {
-    this.cardsMatched.push(cardShellOne);
-    this.cardsMatched.push(cardShellTwo);
-    cardShellOne.classList.add('matched')
-    cardShellTwo.classList.add('matched')
-}
-
-function cardsUnMatched () {
-    this.cardsUnMatched.push(cardShellOne);
-    this.cardsUnMatched.push(cardShellTwo);
-    cardShellOne.classList.add('not matched')
-    cardShellTwo.classList.add('not matched')
-}
-
-function cardShuffle () {
-    cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-}
-
-function cardsUnFlipped () {
-     shellBoard = true;
-
-  setTimeout(() => {
-    cardShellOne.classList.remove('flip');
-    cardShellTwo.classList.remove('flip');
-
-  }, 1500);
-}
-
-
-
- 
-
+card.forEach(card => card.addEventListener('click', cardFlip))
