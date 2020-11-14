818bb5dd53bf43932ac96457d2a3ee35daa2d593
chikos96 <tchikos741@gmail.com>
2020-11-14 15:00:25 +0000

    Testing my code

---
+++ script.js
@@ -0,0 +1,105 @@
+let card = document.querySelectorAll('.inner-card');
+let counter = 0;
+let pairCards = []
+let firstClick = false;
+
+card.forEach((card)=>{
+    card.state = "unclicked"
+}) //  for each state of the card are assigned with a value "unclicked" because they are not clicked.
+
+//Creating a shuffle function
+shuffle ()
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener('click', ()=> {
+        if(!firstClick)(time())
+        firstClick = true
+
+        if(card[i].state == 'unclicked') {
+            card[i].style.transform = 'rotateY(180deg)'
+            card[i].state = 'clicked'
+            counter++
+            pairCards.push(card[i])
+            check()
+
+        }
+
+        else if (card[i].state == 'clicked') {
+            card[i].style.transform = 'rotateY(0deg)'
+            card[i].state = 'unclicked'
+            counter--
+            pairCards = []
+        }
+    })
+
+}
+//Creating a check function
+
+function check() {
+    if (counter==2) {
+        if (pairCards[0].querySelector('img').src==pairCards[i].querySelector('img').src) {
+            matched()  
+        } else {
+            unmatched(pairCards[0], pairCards[1])
+        }
+        
+    }
+}
+
+//If cards match, then the player scores
+function matched(){
+    pairCards[0].state = 'blocked'
+    pairCards[1].state = 'blocked'
+    counter = 0;
+    pairCards = [];
+    let score = document.querySelector('#score').innerHTML
+    score++;
+     document.querySelector('#score').innerHTML = score
+}
+
+//if cards do not match, the player tries again
+function unmatched(){
+    setTimeout(() => {
+        x.style.transform = 'rotateY(0deg)'
+        y.style.transform = 'rotateY(0deg)'
+        
+    }, 850);
+
+    pairCards[0].state = 'unclicked'
+    pairCards[1].state = 'unclicked'
+    counter = 0
+    pairCards = []
+}
+
+//Creeating time function
+function time(){
+    let secs = 0;
+    let SS;
+    setInterval(() => {
+        secs++
+        if(secs==60)
+
+        secs<10?SS=`0${secs}`:SS=`0${secs}`
+
+        document.querySelectorAll('#time').innerHTML = `${SS}`;
+    }, 1000);
+
+}
+//Creating the shuffle function with the images
+function shuffle(){
+    let images = document.querySelectorAll('img')
+    let srcs = ['donatello.jpg','leonardo.png','raphael.jpg','shredder.jpg','splinter.jpg','michelangelo.png','shell-shock.jpg']
+
+    for (let i = srcs.length; i>0; i++) {
+        let j = Math.floor(Math.random() * i);
+        let shell = srcs[i];
+        srcs[i] = srcs[j];
+        srcs[j] = shell;
+    }
+    for (let i = 0; i < images.length ; i++) {
+        images[i].src = srcs[1];
+        
+    }
+
+
+}
